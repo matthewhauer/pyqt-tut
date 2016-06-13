43b5203e097ecb2318c90b19044dd5091a989a69
Matt Hauer <matthew.hauer@gmail.com>
2016-06-13 14:26:39 -0700

    Fixed up button-events.py for pyqt5. Commented out unused imports. Added whitespace per PEP-8 (PyCharm can be obnoxious)

--- pyqt-tut/lesson-two/button-events.py
+++ pyqt-tut/lesson-two/button-events.py
@@ -1,31 +1,40 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+# from PyQt5.QtCore import *
+# from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+
 
 def window():
     app = QApplication(sys.argv)
     win = QDialog()
     b1 = QPushButton(win)
     b1.setText("Button1")
-    b1.move(50,20)
+    b1.move(50, 20)
     b1.clicked.connect(b1_clicked)
 
     b2 = QPushButton(win)
     b2.setText("Button2")
-    b2.move(50,50)
-    QObject.connect(b2,SIGNAL("clicked()"),b2_clicked)
+    b2.move(50, 50)
 
-    win.setGeometry(100,100,200,100)
+    # this syntax no longer works
+    # see http://pyqt.sourceforge.net/Docs/PyQt5/pyqt4_differences.html#old-style-signals-and-slots
+    # QtCore.QObject.connect(b2,QtCore.SIGNAL("clicked()"),b2_clicked)
+
+    b2.clicked.connect(b2_clicked)
+
+    win.setGeometry(100, 100, 200, 100)
     win.setWindowTitle("PyQt")
     win.show()
     sys.exit(app.exec_())
 
+
 def b1_clicked():
     print("Button 1 clicked")
 
+
 def b2_clicked():
     print("Button 2 clicked")
+
 
 if __name__ == '__main__':
     window()
